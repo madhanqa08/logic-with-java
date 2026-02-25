cdad491d529558e924884565930930e5213b6a8c
Madhan <madhanofficial08@gmail.com>
2026-02-25 22:44:00 +0530

    amazon commit

--- src/main/java/Assignment/Amazon.java
+++ src/main/java/Assignment/Amazon.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.time.Duration;
 public class Amazon
 {
@@ -16,15 +19,17 @@ public class Amazon
         options.addArguments("--disable-popup-blocking");
         WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize();
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://www.amazon.in/");
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Bags", Keys.ENTER);
         int position = 6;
         //i[contains(@class,'a-icon a-icon-checkbox')])[position() >= 8]
-        while (position<=9)
+        while (position<=10)
         {
             Actions actions = new Actions(driver);
             WebElement box = driver.findElement(By.xpath("(//i[contains(@class,'a-icon a-icon-checkbox')])[position() >= "+position+"]"));
+            wait.until(ExpectedConditions.elementToBeClickable(box));
             actions.moveToElement(box).build();
             box.click();
             Thread.sleep(6000L);
